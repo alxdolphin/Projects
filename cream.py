c17a8f30d3a48429f931ef0780f9cfa3b94cb46f
kstromz279 <kzs5955@psu.edu>
2024-03-07 20:32:15 -0500

    Added better understanding for own name of bot

--- cream.py
+++ cream.py
@@ -21,6 +21,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    
 
 @client.event
 async def on_message(message):
