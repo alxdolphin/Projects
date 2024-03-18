89edc74787717f6f59501faccc36e70500366ae1
adalton <alexreceivesemails@gmail.com>
2024-03-17 23:29:32 -0400

    Add !flip function

--- cream.py
+++ cream.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Authored-by: Khalil Stroman and Alex Dalton
-
 import os
 import re
 import dotenv as env
 import random
+import discord
 from discord.ext import commands
-# Set API_KEY from .env file
 from dotenv import load_dotenv
+
+# Set API_KEY from .env file
 load_dotenv()
 API_KEY = os.getenv("TOKEN")
-
-
-bot = commands.Bot(commands_prefix = '!')
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+bot = commands.Bot(command_prefix = '!', intents = intents)
 
 @bot.event
 async def on_ready():
@@ -21,20 +23,19 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    
     if message.author == bot.user:
         return  
-      
     elif "cookie" in message.content.lower():
         if str(message.author) == "alxdolphin":
             await message.channel.send("Are you talking to me, sir? Your wish is my command!")
         else:
             await message.channel.send("Are you talking to me? I'm glad to assist!")
 
-@bot.command(name = 'flip', help = 'Flips a coin for Heads or Tails')
-async def coin_flip(ctx):
+    await bot.process_commands(message)
+    
+@bot.command()
+async def flip(ctx):
     options = ['Heads!', 'Tails!']
-
     response = random.choice(options)
     await ctx.send(response)
 
