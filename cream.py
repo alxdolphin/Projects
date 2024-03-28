094964cd0282bfc805af44746fb665743f249a4e
kstromz279 <kzs5955@psu.edu>
2024-03-28 16:00:04 -0400

    Added Music commands

--- cream.py
+++ cream.py
@@ -8,6 +8,9 @@ import random
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import youtube_dl
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 # Set API_KEY from .env file
 load_dotenv()
@@ -32,12 +35,39 @@ async def on_message(message):
             await message.channel.send("Are you talking to me? I'm glad to assist!")
 
     await bot.process_commands(message)
-    
+
+
 @bot.command()
 async def flip(ctx):
     options = ['Heads!', 'Tails!']
     response = random.choice(options)
     await ctx.send(response)
+    
+@bot.command()    
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    if voice_channel and voice_channel.is_connected():
+        await voice_channel.move_to(channel)
+    else:
+        voice_channel = await channel.connect()
 
+@bot.command()
+async def leave(ctx):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    if voice_channel and voice_channel.is_connected():
+        await voice_channel.disconnect()
+
+@bot.command()
+async def play(ctx, url):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    if voice_channel and voice_channel.is_connected():
+        with youtube_dl.YoutubeDL({'format': 'bestaudio'}) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            voice_channel.play(FFmpegPCMAudio(url2))
+            await ctx.send('Playing...')
+
+            
 bot.run(API_KEY)
 
