bbf78d3b14d32bc00194016ea86c29b1ca3a4815
Kstromz279 <kzs5955@psu.edu>
2024-03-31 12:11:41 -0400

    Added to Music Features
    Co-authored-by: Evan Carulli

--- cream.py
+++ cream.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env 
 # -*- coding: utf-8 -*-
 # Authored-by: Khalil Stroman and Alex Dalton
 import os
@@ -11,14 +11,34 @@ from dotenv import load_dotenv
 import youtube_dl
 from discord import FFmpegPCMAudio
 from discord.utils import get
+from collections import deque
+import asyncio
 
 # Set API_KEY from .env file
 load_dotenv()
-API_KEY = os.getenv("TOKEN")
+API_KEY = os.getenv("API_KEY")
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 bot = commands.Bot(command_prefix = '!', intents = intents)
+
+#Music Queue
+servers = {}
+
+def addSong(voiceChannel, url):
+    if voiceChannel in servers:
+        servers[voiceChannel].append(url)
+    else:
+        servers[voiceChannel] = deque()
+        servers[voiceChannel].append(url)
+
+async def play_next(ctx):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    if voice_channel in servers and len(servers[voice_channel]) > 0:
+        url = servers[voice_channel].popleft()
+        await play(ctx, url)
+    
+
 
 @bot.event
 async def on_ready():
@@ -59,15 +79,70 @@ async def leave(ctx):
         await voice_channel.disconnect()
 
 @bot.command()
-async def play(ctx, url):
+async def play(ctx, *args):
+    url = ' '.join(args)
+    channel = ctx.message.author.voice.channel
     voice_channel = get(bot.voice_clients, guild=ctx.guild)
     if voice_channel and voice_channel.is_connected():
-        with youtube_dl.YoutubeDL({'format': 'bestaudio'}) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            voice_channel.play(FFmpegPCMAudio(url2))
-            await ctx.send('Playing...')
+        if not voice_channel.is_playing():
+            with youtube_dl.YoutubeDL({'format': 'bestaudio'}) as ydl:
+                if "youtube.com" in url:
+                    info = ydl.extract_info(url, download=False)
+                    title = info['title']
+                    url2 = info['formats'][0]['url']
+                else:
+                    query = url
+                    info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+                    title = info['title']
+                    url2 = info['formats'][0]['url']
+                voice_channel.play(FFmpegPCMAudio(url2), after=lambda e: asyncio.run_coroutine_threadsafe(play_next(ctx), bot.loop))
+                await ctx.send(f'Playing...{title}')
+        else:
+            with youtube_dl.YoutubeDL({'format': 'bestaudio'}) as ydl:
+                if "youtube.com" in url:
+                    info = ydl.extract_info(url, download=False)
+                    title = info['title']
+                    
+                else:
+                    info = ydl.extract_info(f"ytsearch:{url}", download=False)['entries'][0]
+                    title = info['title']
+                addSong(voice_channel, title)
+                await ctx.send(f'Added to queue...{title}')
 
-            
+    else:
+        voice_channel = await channel.connect()
+        await play(ctx, url)
+    
+
+@bot.command()    
+async def skip(ctx):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    if voice_channel and voice_channel.is_connected():
+        voice_channel.stop()
+        if voice_channel in servers and len(servers[voice_channel]) > 0:
+            url = servers[voice_channel].popleft()
+            await ctx.send('Skipping...')
+            await play(ctx, url)
+        else:
+            await ctx.send('Skipping...')
+
+@bot.command()
+async def queue(ctx):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    curr = list(servers[voice_channel])
+    num = 1
+    for song in curr:
+        await ctx.send(f"{num}. {song}")
+        num += 1
+    
+@bot.command()
+async def remove(ctx, num):
+    voice_channel = get(bot.voice_clients, guild=ctx.guild)
+    curr = list(servers[voice_channel])
+    if int(num) > len(curr):
+        await ctx.send("Invalid number")
+    else:
+        song = curr.pop(int(num)-1)
+        servers[voice_channel] = deque(curr)
+        await ctx.send(f"Removed {song} from queue")
 bot.run(API_KEY)
-
